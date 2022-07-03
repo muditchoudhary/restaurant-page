2f328fe86d7e2bf6b90050548f42372a9b0a78a4
Mudit Choudhary <cmudit60@gmail.com>
2022-07-03 20:51:57 +0530

    Designed the restro first page

--- src/home.js
+++ src/home.js
@@ -1,20 +1,39 @@
 function pageLoad(parent) {
-    const navBar = document.createElement('div');
-    const uoList = document.createElement('ul');
-    navBar.classList.add('navigation')
-    
-    const navLinkTexts = ['Home', 'Menu', 'Contact'];
-    navLinkTexts.forEach((text, i) => {
-        let li = document.createElement('li');
-        let link = document.createElement('a');
-        link.href = 'https://www.google.com/';
-        link.innerText = text;
-        li.appendChild(link);
-        uoList.appendChild(li);
-    })
-    
-    navBar.appendChild(uoList);
-    parent.appendChild(navBar);
+	// header html
+	const header = document.createElement("div");
+	header.classList.add("header");
+
+	// Navigation bar html
+	const navBar = document.createElement("div");
+	const uoList = document.createElement("ul");
+	navBar.classList.add("navigation");
+	const navLinkTexts = ["Home", "Menu", "Contact"];
+	navLinkTexts.forEach((text, i) => {
+		let li = document.createElement("li");
+		let link = document.createElement("a");
+		link.href = "https://www.google.com/";
+		link.innerText = text;
+		li.appendChild(link);
+		uoList.appendChild(li);
+	});
+
+	navBar.appendChild(uoList);
+	header.appendChild(navBar);
+
+	// Restaurant heading html
+	const headingSection = document.createElement("div");
+	headingSection.classList.add("heading-section");
+	const h1 = document.createElement("h1");
+	h1.innerText = "The Indian Masala";
+	const orderBtn = document.createElement("button");
+	orderBtn.innerText = "Order Now";
+
+	headingSection.appendChild(h1);
+	headingSection.appendChild(orderBtn);
+
+	header.appendChild(headingSection);
+
+    parent.appendChild(header);
 }
 
-export {pageLoad};
+export { pageLoad };
