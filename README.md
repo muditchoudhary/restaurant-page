075478aab05f1c5eff3b41b1f41ccd1aad8b917f
Mudit Choudhary <cmudit60@gmail.com>
2022-07-06 15:59:27 +0530

    Made about us section

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 Photo by <a href="https://unsplash.com/@rajat_sarki?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">rajat sarki</a> on <a href="https://unsplash.com/s/photos/indian-dish?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+
+Photo by <a href="https://unsplash.com/@bethdys?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">beth d</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
   
--- src/home.js
+++ src/home.js
@@ -33,7 +33,28 @@ function pageLoad(parent) {
 
 	header.appendChild(headingSection);
 
-    parent.appendChild(header);
+	parent.appendChild(header);
+
+	// About us section
+	const aboutUsSection = document.createElement("div");
+	aboutUsSection.classList.add("about-section");
+	const aboutImgDiv = document.createElement("div");
+	const aboutTextDiv = document.createElement("div");
+
+	const aboutHeading = document.createElement("h2");
+	aboutHeading.innerText = "About us";
+
+	const aboutPara = document.createElement("p");
+	aboutPara.innerText =
+		"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam veritatis reiciendis repudiandae beatae possimus omnis temporibus maiores laudantium veniam ratione illo earum suscipit quibusdam rerum dolor incidunt ad, ipsum accusantium nihil. Eos cupiditate eveniet rerum modi ex natus nemo nobis inventore quidem temporibus, labore maxime eum aliquam magni ipsam voluptate ducimus deserunt. Fugiat tenetur quas earum? Quasi quod blanditiis a eos, natus possimus quis officiis dolore rem aliquam corporis laboriosam in";
+
+	aboutTextDiv.appendChild(aboutHeading);
+	aboutTextDiv.appendChild(aboutPara);
+
+	aboutUsSection.appendChild(aboutImgDiv);
+	aboutUsSection.appendChild(aboutTextDiv);
+
+	parent.appendChild(aboutUsSection);
 }
 
 export { pageLoad };
