c09f9961d6039f096873f795389ce97edd3743cf
Mudit Choudhary <cmudit60@gmail.com>
2022-07-07 13:00:27 +0530

    Made the footer section

--- README.md
+++ README.md
@@ -2,4 +2,9 @@ Photo by <a href="https://unsplash.com/@rajat_sarki?utm_source=unsplash&utm_medi
 
 Photo by <a href="https://unsplash.com/@bethdys?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">beth d</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
   
+<a href="https://www.flaticon.com/free-icons/twitter" title="twitter icons">Twitter icons created by Pixel perfect - Flaticon</a>
+
+<a href="https://www.flaticon.com/free-icons/linkedin" title="linkedin icons">Linkedin icons created by riajulislam - Flaticon</a>
+
+<a href="https://www.flaticon.com/free-icons/github" title="github icons">Github icons created by Dave Gandy - Flaticon</a>
   
--- src/home.js
+++ src/home.js
@@ -1,3 +1,7 @@
+import twitterIcon from "./assets/twitter.png";
+import linkedinIcon from "./assets/linkedin.png";
+import githubIcon from "./assets/github-sign.png";
+
 function pageLoad(parent) {
 	// header html
 	const header = document.createElement("div");
@@ -8,14 +12,7 @@ function pageLoad(parent) {
 	const uoList = document.createElement("ul");
 	navBar.classList.add("navigation");
 	const navLinkTexts = ["Home", "Menu", "Contact"];
-	navLinkTexts.forEach((text, i) => {
-		let li = document.createElement("li");
-		let link = document.createElement("a");
-		link.href = "https://www.google.com/";
-		link.innerText = text;
-		li.appendChild(link);
-		uoList.appendChild(li);
-	});
+	createSectionLinks(navLinkTexts, uoList);
 
 	navBar.appendChild(uoList);
 	header.appendChild(navBar);
@@ -55,6 +52,66 @@ function pageLoad(parent) {
 	aboutUsSection.appendChild(aboutTextDiv);
 
 	parent.appendChild(aboutUsSection);
+
+	// Footer section
+	const footerSection = document.createElement("div");
+	footerSection.classList.add("footer");
+
+	const upperPart = document.createElement("div");
+	upperPart.classList.add("upper-footer");
+
+	const lowerPart = document.createElement("div");
+	lowerPart.classList.add("lower-footer");
+
+	const linksSection = document.createElement("div");
+	const h3 = document.createElement("h3");
+	h3.innerText = "Section";
+	const ul = document.createElement("ul");
+	createSectionLinks(navLinkTexts, ul);
+	linksSection.appendChild(h3);
+	linksSection.appendChild(ul);
+
+	const addressSection = document.createElement("div");
+	const addressH3 = document.createElement("h3");
+	addressH3.innerText = "Address";
+	const addressContent = document.createElement("p");
+	addressContent.innerText =
+		"The Indian Masala\n B-18 Super Complex\n Rajat Nagar, Banglore\n India";
+	addressSection.appendChild(addressH3);
+	addressSection.appendChild(addressContent);
+
+	const timingSection = document.createElement("div");
+	const timingH3 = document.createElement("h3");
+	timingH3.innerText = "Timings";
+	const timingDetails = document.createElement("div");
+	timingDetails.innerText =
+		"Weekdays\n11:00 AM - 10:00 PM\nWeekends\n11:00 AM - 12:00 AM";
+	timingSection.appendChild(timingH3);
+	timingSection.appendChild(timingDetails);
+
+	lowerPart.innerText =
+		"\u00A9 2022 The Indian Masala. All Right Reserved";
+
+	upperPart.appendChild(linksSection);
+	upperPart.appendChild(addressSection);
+	upperPart.appendChild(timingSection);
+
+
+	footerSection.appendChild(upperPart);
+	footerSection.appendChild(lowerPart);
+
+	parent.appendChild(footerSection);
+}
+
+function createSectionLinks(list, parentList) {
+	list.forEach((text, i) => {
+		let li = document.createElement("li");
+		let link = document.createElement("a");
+		link.href = "https://www.google.com/";
+		link.innerText = text;
+		li.appendChild(link);
+		parentList.appendChild(li);
+	});
 }
 
 export { pageLoad };
